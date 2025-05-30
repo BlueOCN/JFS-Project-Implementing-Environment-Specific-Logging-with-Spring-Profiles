4743e0c388537a3ae429c3d8f789bc16cf325779
BlueOCN <robgonzalezmu@gmail.com>
2025-05-30 17:44:07 -0600

    Update - Step 1: Set Up the Project Completed
    Dependencies:
    - Spring Web
    - Spring Boot Actuator
    
    IDE:
    - IntelliJ IDEA

---
+++ Environment-Specific-Logging/src/main/java/com/bigocn/Environment_Specific_Logging/EnvironmentSpecificLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.bigocn.Environment_Specific_Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentSpecificLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentSpecificLoggingApplication.class, args);
+	}
+
+}
---
+++ Environment-Specific-Logging/src/test/java/com/bigocn/Environment_Specific_Logging/EnvironmentSpecificLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigocn.Environment_Specific_Logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentSpecificLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
